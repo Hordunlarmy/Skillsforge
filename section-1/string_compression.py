1d8d9409a28c04cc91b471c23e3930791f9d1fd4
hordunlarmy <hordunlarmy@gmail.com>
2024-04-25 06:38:52 +0000

    Add string compression function with test cases

---
+++ section-1/string_compression.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def compress(string):
+    """ Compress a string """
+
+    compressed = []
+    current = string[0]
+    no_of_char = 0
+    for char in string:
+        if char == current:
+            no_of_char += 1
+        else:
+            if no_of_char > 1:
+                compressed.append(current + str(no_of_char))
+            else:
+                compressed.append(current)
+            current = char
+            no_of_char = 1
+    if no_of_char > 1:
+        compressed.append(current + str(no_of_char))
+    else:
+        compressed.append(current)
+
+    compressed_string = ''.join(compressed)
+
+    if len(compressed_string) < string:
+        return compressed_string
+    else:
+        return string
+
+
+# Test cases
+assert compress('bbcceeee') == 'b2c2e4'
+assert compress('aaabbbcccaaa') == 'a3b3c3a3'
+assert compress('a') == 'a'
+
+print("All tests passed successfully!")
