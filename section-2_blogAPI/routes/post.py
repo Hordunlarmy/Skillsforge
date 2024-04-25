bdab517979919d4e8fbacc321996c1b1c1949980
hordunlarmy <hordunlarmy@gmail.com>
2024-04-25 17:45:42 +0000

    improve post deletion and update logi

--- section-2_blogAPI/routes/post.py
+++ section-2_blogAPI/routes/post.py
@@ -10,81 +10,6 @@ from auth import user_dependency
 post = APIRouter()
 
 
-@post.get("/posts/{post_id}", response_model=Post)
-async def read_post(current_user: user_dependency,
-                    post_id: str = Path(...,
-                                        description="The ID of the post"
-                                        " to retrieve"),
-                    db: Session = Depends(get_db)):
-    """ Retrieve a post """
-
-    post = db.query(models.Post).filter(models.Post.id == post_id).first()
-    if not post:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return post
-
-
-@post.put("/posts/{post_id}", response_model=Post)
-async def update_post(current_user: user_dependency, post: PostCreate,
-                      post_id: str = Path(...,
-                                          description="The ID of the post"
-                                          " to update"),
-                      db: Session = Depends(get_db)):
-    """ Update a post """
-
-    post_to_update = db.query(models.Post).filter(
-        models.Post.id == post_id).first()
-    if post_to_update.user_id != current_user.id:
-        raise HTTPException(
-            status_code=404, detail="You cant update another user's post")
-    if not post_to_update:
-        raise HTTPException(status_code=404, detail="post doesnt exist")
-
-    post_to_update.title = post.title
-    post_to_update.content = post.content
-    post_to_update.date_posted = datetime.utcnow()
-
-    try:
-        db.commit()
-        db.refresh(post_to_update)
-    except Exception as e:
-        db.rollback()
-        raise HTTPException(
-            status_code=400, detail=str(e))
-
-    return post_to_update
-
-
-@post.delete("/posts/{post_id}")
-async def delete_post(current_user: user_dependency,
-                      post_id: str = Path(...,
-                                          description="The ID of the post"
-                                          " to delete"),
-                      db: Session = Depends(get_db)):
-    """ Delete a post by its id """
-
-    post_to_delete = db.query(models.Post).filter(
-        models.Post.id == post_id).first()
-
-    if post_to_delete.user_id != current_user.id:
-        raise HTTPException(
-            status_code=404, detail="You cant delete another user's post")
-
-    if post_to_delete is None:
-        raise HTTPException(status_code=404, detail="Post doesnt exist")
-    deleted_id = post_to_delete.id
-
-    try:
-        db.delete(post_to_delete)
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        raise HTTPException(
-            status_code=400, detail=str(e))
-
-    return {"message": f"Post {deleted_id} deleted successfully"}
-
-
 @post.post("/posts/", response_model=Post)
 async def create_post(current_user: user_dependency, post: PostCreate,
                       db: Session = Depends(get_db)):
@@ -119,3 +44,79 @@ async def read_posts(current_user: user_dependency,
     if not posts:
         raise HTTPException(status_code=404, detail="No posts found")
     return [schemas.Post.from_orm(post) for post in posts]
+
+
+@post.get("/posts/{post_id}", response_model=Post)
+async def read_post(current_user: user_dependency,
+                    post_id: str = Path(...,
+                                        description="The ID of the post"
+                                        " to retrieve"),
+                    db: Session = Depends(get_db)):
+    """ Retrieve a post """
+
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+
+@post.put("/posts/{post_id}", response_model=Post)
+async def update_post(current_user: user_dependency, post: PostCreate,
+                      post_id: str = Path(...,
+                                          description="The ID of the post"
+                                          " to update"),
+                      db: Session = Depends(get_db)):
+    """ Update a post """
+
+    post_to_update = db.query(models.Post).filter(
+        models.Post.id == post_id).first()
+
+    if not post_to_update:
+        raise HTTPException(status_code=404, detail="post doesnt exist")
+
+    if post_to_update.user_id != current_user.id:
+        raise HTTPException(
+            status_code=404, detail="You cant update another user's post")
+        post_to_update.title = post.title
+    post_to_update.content = post.content
+    post_to_update.date_posted = datetime.utcnow()
+
+    try:
+        db.commit()
+        db.refresh(post_to_update)
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=400, detail=str(e))
+
+    return post_to_update
+
+
+@post.delete("/posts/{post_id}")
+async def delete_post(current_user: user_dependency,
+                      post_id: str = Path(...,
+                                          description="The ID of the post"
+                                          " to delete"),
+                      db: Session = Depends(get_db)):
+    """ Delete a post by its id """
+
+    post_to_delete = db.query(models.Post).filter(
+        models.Post.id == post_id).first()
+
+    if post_to_delete is None:
+        raise HTTPException(status_code=404, detail="Post doesnt exist")
+    deleted_id = post_to_delete.id
+
+    if post_to_delete.user_id != current_user.id:
+        raise HTTPException(
+            status_code=404, detail="You cant delete another user's post")
+
+    try:
+        db.delete(post_to_delete)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=400, detail=str(e))
+
+    return {"message": f"Post {deleted_id} deleted successfully"}
